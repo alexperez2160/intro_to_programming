455178430c719d639146defe41f51b61c7bb9efd
alexperez2160 <alexperez2160@gmail.com>
2016-05-08 21:15:30 +0000

    changed title

---
+++ basics.rb
@@ -0,0 +1,48 @@
+##1 
+
+puts "Alejandro"+"Perez"
+
+##2
+thousands = 5347 / 1000
+hundreds = 5347 % 1000 / 100
+tens = 5347 % 1000 % 100 / 10
+ones = 5347 % 1000 %100 % 10
+
+puts thousands 
+puts hundreds 
+puts tens 
+puts ones 
+
+##3
+
+movies= {godfather:1967, jurassic:2015, meet:2013}
+puts movies[:godfather]
+puts movies[:jurassic]
+puts movies[:meet]
+
+##4
+dates=[1967, 2015, 2013]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+
+##5 
+x= 5*4*3*2*1
+puts x
+y= 6*5*4*3*2*1
+puts y 
+z=7*6*5*4*3*2*1
+puts z
+d=8*7*6*5*4*3*2*1
+puts d
+
+##6
+a= 14.56*14.56
+puts a 
+
+b= 5.5*5.5
+puts b 
+
+c=6.578*6.578
+puts c
+
