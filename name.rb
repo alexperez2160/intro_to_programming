27ae5a9ec06efd6abf48a494d62471630fabe2fe
alexperez2160 <alexperez2160@gmail.com>
2016-05-08 23:06:53 +0000

    finishevariable

---
+++ name.rb
@@ -0,0 +1,26 @@
+## name.rb
+puts "What is your first name?"
+first_name= gets.chomp
+puts "Hello"+first_name
+puts "What is your last name?"
+last_name=gets.chomp
+puts first_name+last_name
+
+##
+
+##10.times do 
+  ##  puts name 
+##end 
+
+##x = 0
+##3.times do
+  ##x += 1
+##end
+##puts x
+
+y = 0
+3.times do
+  y += 1
+  x = y
+end
+puts x
